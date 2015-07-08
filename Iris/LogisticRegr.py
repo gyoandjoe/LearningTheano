207e4feec0b818e3ec6cffaad59e6de60782a1af
gyoandjoe <win-g@hotmail.com>
2015-07-07 22:18:35 -0500

    Testing Examples for softmax

--- Iris/LogisticRegr.py
+++ Iris/LogisticRegr.py
@@ -1,10 +1,15 @@
 __author__ = 'Giovanni'
 import theano
+import theano.tensor as T
 import numpy as np
 
 class LogisticRegr(object):
 
+    #n_in  = No de Variables
+    #n_out = No de clases
+    #input = DataSet sin valores de respuesta
     def __init__(self, input, n_in, n_out):
+
         self.W = theano.shared(
             value=np.zeros(
                 (n_in, n_out),
@@ -22,3 +27,55 @@ class LogisticRegr(object):
             name='b',
             borrow=True
         )
+
+        self.p_y_given_x = T.nnet.softmax(T.dot(input, self.W) + self.b)
+        self.y_pred = T.argmax(self.p_y_given_x, axis=1)
+        self.params = [self.W, self.b]
+
+    def negative_log_likelihood(self, y):
+        """
+        Cost Function with mean and not sum
+
+        Return the mean of the negative log-likelihood of the prediction
+        of this model under a given target distribution.
+
+        .. math::
+
+            \frac{1}{|\mathcal{D}|} \mathcal{L} (\theta=\{W,b\}, \mathcal{D}) =
+            \frac{1}{|\mathcal{D}|} \sum_{i=0}^{|\mathcal{D}|}
+                \log(P(Y=y^{(i)}|x^{(i)}, W,b)) \\
+            \ell (\theta=\{W,b\}, \mathcal{D})
+
+        :type y: theano.tensor.TensorType
+        :param y: corresponds to a vector that gives for each example the
+                  correct label
+
+        Note: we use the mean instead of the sum so that
+              the learning rate is less dependent on the batch size
+        """
+        # start-snippet-2
+        # y.shape[0]
+        # is (symbolically) the number of rows in y, i.e.,
+        # number of examples (call it n) in the minibatch
+        #
+        # T.arange(y.shape[0])
+        # is a symbolic vector which will contain
+        # [0,1,2,... n-1]
+        #
+        # T.log(self.p_y_given_x)
+        # is a matrix of
+        # Log-Probabilities (call it LP) with one row per example and
+        # one column per class
+        #
+        # LP[T.arange(y.shape[0]),y]
+        # is a vector
+        # v containing [LP[0,y[0]], LP[1,y[1]], LP[2,y[2]], ..., LP[n-1,y[n-1]]]
+        #
+        # and
+        #
+        # T.mean(LP[T.arange(y.shape[0]),y])
+        # isthe mean (across minibatch examples) of the elements in v,
+        # i.e., the mean log-likelihood across the minibatch.
+
+        return -T.sumu(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
+        # end-snippet-2
--- Iris/Main.py
+++ Iris/Main.py
@@ -31,15 +31,35 @@ classifier = LogisticRegr.LogisticRegr(input=x, n_in=4, n_out=3)
 
 
 
-W = theano.shared(
-    value=np.zeros(
-        (4, 3),
-        dtype=theano.config.floatX
-        ),
-        name='W',
-        borrow=True
-    )
 
-test = T.nnet.softmax(T.dot(x, W))
+#mm = train_set_x.get_value(borrow=True)
+#W = theano.shared(
+#    value=np.ones(
+#        (90, 4),
+#        dtype=theano.config.floatX
+#        ),
+#        name='W',
+#        borrow=True
+#    )
+#num1 = T.scalar()
+#num2 = T.scalar()
+#Wm = T.mul(x,num2)
+#wres = theano.function([x,num2],Wm)
+#rr = wres(train_set_x.get_value(),5)
+
+############
+testMat = np.array([[1000,815,815,815,815],[20,20,20,20,21],[0.5,0.7,0.4,0.6,0.1],[10,10,70,5,5]],dtype=theano.config.floatX)
+test = T.nnet.softmax(x)
+testF = theano.function([x],test)
+testRes = testF(testMat)
+
+#MaxSim = T.argmax(test, axis=1)
+#MaxF = theano.function([test],MaxSim)
+#resMax = MaxF(testRes)
+
+logSim = T.log(test)
+logF = theano.function([test],logSim)
+logRes = logF(testRes)
+
 
 print ('Ok Main')
